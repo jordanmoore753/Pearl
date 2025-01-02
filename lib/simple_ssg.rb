b5ada916fd163dc5f706826d8f865280ea40397b
Jordan Moore <jordanmoore753@gmail.com>
2025-01-02 16:54:11 -0500

    rb: create first method

--- lib/simple_ssg.rb
+++ lib/simple_ssg.rb
@@ -5,4 +5,19 @@ require_relative "simple_ssg/version"
 module SimpleSsg
   class Error < StandardError; end
   # Your code goes here...
+
+  LAYOUT_TEMPLATE = <<~HAML
+    !!!
+    %html
+      %head
+        %title
+      %body
+        = yield
+  HAML
+
+  def create_layout(name:)
+    @name = name
+
+    File.write("layouts/#{name}.haml", LAYOUT_TEMPLATE)
+  end
 end
