cc0a4a0de74de0b1c047663c968c645baf57a81a
Jordan Moore <jordanmoore753@gmail.com>
2025-01-04 15:41:52 -0500

    add reporters to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,9 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "pearl"
 
-require "minitest/autorun"
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'fileutils'
+require 'tempfile'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new(time: true)
